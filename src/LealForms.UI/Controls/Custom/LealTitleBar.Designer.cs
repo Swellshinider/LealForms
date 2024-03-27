1ba7d9ef17e2501800ebc9eda674d8da6603358a
Swellshinider <eduardoleal.contact@gmail.com>
2024-03-27 17:26:37 -0300

    custom titlebar

---
+++ src/LealForms.UI/Controls/Custom/LealTitleBar.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace LealForms.UI.Controls.Custom
+{
+    partial class LealTitleBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonClose = new Button();
+            buttonMaxNor = new Button();
+            buttonMin = new Button();
+            panelTop = new Panel();
+            labelTitle = new Label();
+            labelIcon = new Label();
+            panelTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            buttonClose.Dock = DockStyle.Right;
+            buttonClose.FlatAppearance.BorderSize = 0;
+            buttonClose.FlatStyle = FlatStyle.Flat;
+            buttonClose.Image = Resources.ResourceMngr.close_white_theme;
+            buttonClose.Location = new Point(531, 0);
+            buttonClose.Name = "buttonClose";
+            buttonClose.Size = new Size(48, 32);
+            buttonClose.TabIndex = 0;
+            buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // buttonMaxNor
+            // 
+            buttonMaxNor.Dock = DockStyle.Right;
+            buttonMaxNor.FlatAppearance.BorderSize = 0;
+            buttonMaxNor.FlatStyle = FlatStyle.Flat;
+            buttonMaxNor.Image = Resources.ResourceMngr.maximize_white_theme;
+            buttonMaxNor.Location = new Point(483, 0);
+            buttonMaxNor.Name = "buttonMaxNor";
+            buttonMaxNor.Size = new Size(48, 32);
+            buttonMaxNor.TabIndex = 1;
+            buttonMaxNor.UseVisualStyleBackColor = true;
+            // 
+            // buttonMin
+            // 
+            buttonMin.Dock = DockStyle.Right;
+            buttonMin.FlatAppearance.BorderSize = 0;
+            buttonMin.FlatStyle = FlatStyle.Flat;
+            buttonMin.Image = Resources.ResourceMngr.minimize_white_theme;
+            buttonMin.Location = new Point(435, 0);
+            buttonMin.Name = "buttonMin";
+            buttonMin.Size = new Size(48, 32);
+            buttonMin.TabIndex = 2;
+            buttonMin.UseVisualStyleBackColor = true;
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(labelTitle);
+            panelTop.Controls.Add(labelIcon);
+            panelTop.Dock = DockStyle.Fill;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(435, 32);
+            panelTop.TabIndex = 3;
+            // 
+            // labelTitle
+            // 
+            labelTitle.Dock = DockStyle.Fill;
+            labelTitle.Location = new Point(48, 0);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(387, 32);
+            labelTitle.TabIndex = 1;
+            labelTitle.Text = "Title";
+            labelTitle.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // labelIcon
+            // 
+            labelIcon.Dock = DockStyle.Left;
+            labelIcon.Location = new Point(0, 0);
+            labelIcon.Name = "labelIcon";
+            labelIcon.Size = new Size(48, 32);
+            labelIcon.TabIndex = 0;
+            // 
+            // LealTitleBar
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = SystemColors.Control;
+            Controls.Add(panelTop);
+            Controls.Add(buttonMin);
+            Controls.Add(buttonMaxNor);
+            Controls.Add(buttonClose);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            MaximumSize = new Size(100000, 32);
+            MinimumSize = new Size(144, 32);
+            Name = "LealTitleBar";
+            Size = new Size(579, 32);
+            Resize += LealTitleBar_Resize;
+            ParentChanged += LealTitleBar_ParentChanged;
+            panelTop.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonClose;
+        private Button buttonMaxNor;
+        private Button buttonMin;
+        private Panel panelTop;
+        private Label labelIcon;
+        private Label labelTitle;
+    }
+}
---
+++ src/LealForms.UI/Controls/Custom/LealTitleBar.cs
@@ -0,0 +1,118 @@
+﻿using LealForms.UI.Resources;
+using LealForms.UI.Utils.Extensions;
+using System.ComponentModel;
+
+namespace LealForms.UI.Controls.Custom
+{
+    public partial class LealTitleBar : UserControl
+    {
+        private bool _blackTheme = false;
+
+        public LealTitleBar()
+        {
+            // Enable double buffering to reduce flicker and improve rendering performance.
+            DoubleBuffered = true;
+
+            // Ensure the form is redrawn when resized.
+            SetStyle(ControlStyles.ResizeRedraw, true);
+
+            InitializeComponent();
+            InitializeObjects();
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public bool BlackTheme
+        {
+            get => _blackTheme;
+            set
+            {
+                _blackTheme = value;
+                UpdateIconsAndColors();
+            }
+        }
+
+        private void InitializeObjects()
+        {
+            buttonClose.FlatAppearance.MouseOverBackColor = Color.Red;
+
+            buttonMin.Click += ButtonMin_Click;
+            buttonMaxNor.Click += ButtonMaxNor_Click;
+            buttonClose.Click += ButtonClose_Click;
+            panelTop.MouseDown += ControlMouseDown;
+            labelIcon.MouseDown += ControlMouseDown;
+            labelTitle.MouseDown += ControlMouseDown;
+            UpdateIconsAndColors();
+        }
+
+        private void UpdateIconsAndColors()
+        {
+            buttonClose.Image = _blackTheme ? ResourceMngr.close_black_theme : ResourceMngr.close_white_theme;
+            buttonMaxNor.Image = _blackTheme ? ResourceMngr.normalize_black_theme : ResourceMngr.maximize_black_theme;
+            buttonMin.Image = _blackTheme ? ResourceMngr.minimize_black_theme : ResourceMngr.minimize_white_theme;
+
+            if (Parent == null)
+                return;
+
+            var maximized = ((Form)Parent).WindowState == FormWindowState.Maximized;
+
+            buttonMaxNor.Image = _blackTheme 
+                    ? maximized 
+                                ? ResourceMngr.normalize_black_theme 
+                                : ResourceMngr.maximize_black_theme
+                    : maximized 
+                                ? ResourceMngr.normalize_white_theme 
+                                : ResourceMngr.maximize_white_theme;
+
+            if (Parent is Form parent)
+            {
+                labelTitle.Text = Parent.Text;
+
+                if (parent.Icon != null)
+                    labelIcon.Image = parent.Icon.ToBitmap();
+            }
+        }
+
+        private void LealTitleBar_ParentChanged(object sender, EventArgs e)
+        {
+            if (Parent == null)
+                return;
+
+            if (Parent is not Form)
+                throw new ApplicationException("Invalid Parent type for LealTitleBar. LealTitleBar require a Form as Parent");
+
+            BackColor = Parent.BackColor;
+            UpdateIconsAndColors();
+        }
+
+        private void LealTitleBar_Resize(object? sender, EventArgs e) => UpdateIconsAndColors();
+
+        private void ButtonMin_Click(object? sender, EventArgs e)
+        {
+            if (Parent is Form form)
+                form.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonMaxNor_Click(object? sender, EventArgs e)
+        {
+            if (Parent is Form form)
+            {
+                var maximized = form.WindowState == FormWindowState.Maximized;
+                form.WindowState = maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+            }   
+        }
+
+        private void ButtonClose_Click(object? sender, EventArgs e)
+        {
+            if (Parent is Form form)
+                form.Close();
+        }
+
+        private void ControlMouseDown(object? sender, MouseEventArgs e)
+        {
+            if (Parent == null)
+                return;
+
+            Parent.Handle.ControlMouseDown(e);
+        }
+    }
+}
