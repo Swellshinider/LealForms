1acd24ed59c2cdb681a56e96616fca35be7c73b7
Swellshinider <eduardoleal.contact@gmail.com>
2024-03-27 12:46:36 -0300

    basic example

---
+++ src/LealForms.Examples/Program.cs
@@ -0,0 +1,19 @@
+using LealForms.UI.Forms;
+
+namespace LealForms.Examples
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        internal static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LealBorderlessForm());
+        }
+    }
+}
