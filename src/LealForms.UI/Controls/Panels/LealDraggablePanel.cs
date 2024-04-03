0dac3ac929cc34264c9dffb9ba4238f87cee5a0b
Swellshinider <eduardoleal.contact@gmail.com>
2024-04-03 09:47:32 -0300

    problem fixed

--- src/LealForms.UI/Controls/Panels/LealDraggablePanel.cs
+++ src/LealForms.UI/Controls/Panels/LealDraggablePanel.cs
@@ -11,28 +11,12 @@ namespace LealForms.UI.Controls.Panels
         public LealDraggablePanel()
         {
             MouseDown += LealDraggablePanel_MouseDown;
-            ParentChanged += LealDraggablePanel_ParentChanged;
-        }
-
-        /// <summary>
-        /// Updates the panel's background color to match its parent's background color.
-        /// </summary>
-        private void LealDraggablePanel_ParentChanged(object? sender, EventArgs e)
-        {
-            if (Parent != null)
-                BackColor = Parent.BackColor;
         }
 
         /// <summary>
         /// Handles the MouseDown event to initiate dragging of the panel.
         /// </summary>
         private void LealDraggablePanel_MouseDown(object? sender, MouseEventArgs e)
-        {
-            if (Parent == null)
-                Handle.ControlMouseDown(e);
-            else
-                Parent.Handle.ControlMouseDown(e);
-
-        }
+            => this.FindParentForm()?.Handle.ControlMouseDown(e);
     }
 }
